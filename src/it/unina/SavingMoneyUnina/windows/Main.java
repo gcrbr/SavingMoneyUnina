3d960bea1f61a2fa257d776d82d7180794a1ff34
Sasy <carleosalvatore@gmail.com>
2023-12-14 11:33:11 +0100

    Aggiunto login

--- src/it/unina/SavingMoneyUnina/windows/Main.java
+++ src/it/unina/SavingMoneyUnina/windows/Main.java
@@ -6,11 +6,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Color;
+import javax.swing.JLabel;
 
 public class Main extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField email;
+	private JPasswordField password;
 
 	/**
 	 * Launch the application.
@@ -34,14 +42,38 @@ public class Main extends JFrame {
 	public Main() {
 		setTitle("Accedi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 772, 351);
+		setBounds(100, 100, 250, 314);
 		contentPane = new JPanel();
+		contentPane.setForeground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		
-		JButton btnNewButton = new JButton("New button");
-		contentPane.add(btnNewButton);
+		JButton Accedi = new JButton("Accedi");
+		Accedi.setBounds(21, 140, 78, 21);
+		Accedi.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		
+		email = new JTextField();
+		email.setBounds(21, 62, 96, 19);
+		contentPane.add(email);
+		email.setColumns(10);
+		
+		password = new JPasswordField();
+		password.setBackground(new Color(255, 255, 255));
+		password.setBounds(21, 108, 96, 19);
+		contentPane.add(password);
+		contentPane.add(Accedi);
+		
+		JLabel lblNewLabel = new JLabel("Email");
+		lblNewLabel.setBounds(21, 39, 96, 19);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setBounds(21, 85, 96, 13);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
