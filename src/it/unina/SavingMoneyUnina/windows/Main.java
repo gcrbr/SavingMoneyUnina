8ffab885cb53469a2d4546f74d857d3114149c2f
Sasy <carleosalvatore@gmail.com>
2023-12-14 11:04:06 +0100

    Prova

--- src/it/unina/SavingMoneyUnina/windows/Main.java
+++ src/it/unina/SavingMoneyUnina/windows/Main.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class Main extends JFrame {
 
@@ -31,12 +32,16 @@ public class Main extends JFrame {
 	 * Create the frame.
 	 */
 	public Main() {
+		setTitle("Accedi");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 772, 351);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		
+		JButton btnNewButton = new JButton("New button");
+		contentPane.add(btnNewButton);
 	}
 
 }
