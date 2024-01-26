5b5ff55371ddecbb425c1821debc5c2bc137a977
gcrbr <76233939+gcrbr@users.noreply.github.com>
2024-01-26 15:48:11 +0100

    creazione entità Categoria e Portafogli

--- src/main/java/it/unina/maven/SavingMoneyUnina/entities/Categoria.java
+++ src/main/java/it/unina/maven/SavingMoneyUnina/entities/Categoria.java
@@ -2,4 +2,17 @@ package it.unina.maven.SavingMoneyUnina.entities;
 
 public class Categoria {
 
+	private String nome;
+	
+	public Categoria(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
--- src/main/java/it/unina/maven/SavingMoneyUnina/entities/Portafogli.java
+++ src/main/java/it/unina/maven/SavingMoneyUnina/entities/Portafogli.java
@@ -1,5 +1,22 @@
 package it.unina.maven.SavingMoneyUnina.entities;
 
 public class Portafogli {
-
+	private int idportafogli;
+	private String nome;
+	
+	public int getIdportafogli() {
+		return idportafogli;
+	}
+	
+	public void setIdportafogli(int idportafogli) {
+		this.idportafogli = idportafogli;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
