ace4a2ca03987d4bcafca88429b2e6e49cb4c116
gcrbr <76233939+gcrbr@users.noreply.github.com>
2024-01-26 15:59:29 +0100

    creazione classe Carta

--- src/main/java/it/unina/maven/SavingMoneyUnina/entities/Carta.java
+++ src/main/java/it/unina/maven/SavingMoneyUnina/entities/Carta.java
@@ -1,5 +1,69 @@
 package it.unina.maven.SavingMoneyUnina.entities;
 
-public class Carta {
+import java.util.Date;
 
+public class Carta {
+	private String numero;
+	private Date scadenza;
+	private String cvv;
+	private String tipo;
+	private double plafond;
+	private double limitespesa;
+	
+	public String getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public Date getScadenza() {
+		return scadenza;
+	}
+	
+	public void setScadenza(Date scadenza) {
+		this.scadenza = scadenza;
+	}
+	
+	public String getCvv() {
+		return cvv;
+	}
+	
+	public void setCvv(String cvv) {
+		this.cvv = cvv;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public double getPlafond() {
+		return plafond;
+	}
+	
+	public void setPlafond(double plafond) {
+		this.plafond = plafond;
+	}
+	
+	public double getLimitespesa() {
+		return limitespesa;
+	}
+	
+	public void setLimitespesa(double limitespesa) {
+		this.limitespesa = limitespesa;
+	}
+	
+	public boolean isCredito() {
+		return getTipo().equals("credito");
+	}
+	
+	public boolean isDebito() {
+		return getTipo().equals("debito");
+	}
+	
 }
